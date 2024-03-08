9e89211c31b3475cdceadf06bb14c72d8629adfb
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-03-08 23:37:44 +0700

    Update: Telegram

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,19 @@ Route::get('/gemini-pro', function () {
     return response()->view('gemini-pro');
 });
 
+Route::get('/telegram-webhook', function () {
+    return response()->json(['message' => 'welcome!']);
+});
+
+Route::post('/telegram-webhook', function (Request $request) {
+    if ($request->header('X-Telegram-Bot-Api-Secret-Token') == 'berserk') {
+
+        $file = fopen(storage_path('app/public/Request.json'), 'w');
+        fwrite($file, json_encode($request->all(), JSON_PRETTY_PRINT));
+        fclose($file);
+
+        return response()->json($request->all());
+    } else {
+        return response(['ok' => false], 404);
+    }
+});
