83275366958d379c02d4e67ff89e90d93ad94e7a
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-05-07 23:45:31 +0700

    Update: API FE

--- resources/views/livewire/api.blade.php
+++ resources/views/livewire/api.blade.php
@@ -6,22 +6,116 @@
                 class="text-5xl font-bold before:w-64 before:h-64 before:bg-gradient-to-br before:from-red-500 before:from-50% before:to-pink-vite before:to-50% before:absolute before:-z-50 before:top-[50%] before:left-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:blur-[90px]">API</span>
         </div>
         <div
-            class="col-span-12 row-span-3 md:col-span-8 md:row-span-12 bg-white/5 backdrop-blur-sm rounded p-5 flex flex-col before:w-56 before:h-56 before:bg-gradient-to-br before:from-blue-vite before:from-50% before:to-green-500 before:to-50% before:absolute before:-z-50 before:top-[50%] before:left-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:blur-[170px]">
-            <div>
-                <span class="w-auto whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-lg text-blue-700">
-                    GET
-                </span>
-                <p class="bg-gradient-to-r from-zinc-700 to-black px-4 py-2 rounded-md">
-                    https://ardiputra.us.to/get/image?key=WSn6CU0Zsxa0nPhlEK1dct
-                </p>
+            class="col-span-12 row-span-3 md:col-span-8 md:row-span-12 bg-white/5 backdrop-blur-sm rounded p-5 flex flex-col justify-between before:w-56 before:h-56 before:bg-gradient-to-br before:from-pink-vite before:from-50% before:to-blue-vite before:to-50% before:absolute before:-z-50 before:top-[50%] before:left-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:blur-[170px]">
+            <div class="flex flex-col gap-2">
+                <div class="flex gap-2">
+                    <span class="w-auto whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-sm text-blue-700">
+                        GET
+                    </span>
+                    <span
+                        class="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                        </svg>
+
+                        <p class="whitespace-nowrap text-sm">Text AI</p>
+                    </span>
+                    <span class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-purple-700">
+                        : JSON
+                    </span>
+                </div>
+                <div class="bg-gradient-to-r from-zinc-700 to-black px-4 py-2 rounded-2xl shadow-lg shadow-black">
+                    https://ardiputra.com/get/text?<span class="text-yellow-500">key</span>=<span
+                        class="text-blue-vite">WSn6CU0Zsxa0nPhlEK1dct</span>&<span
+                        class="text-yellow-500">message</span>=<span class="text-blue-vite">how to
+                        center a div</span>
+                </div>
             </div>
-            <div>
-                <span class="w-auto whitespace-nowrap rounded-full bg-green-100 px-2.5 py-0.5 text-lg text-green-700">
-                    POST
-                </span>
-                <p class="bg-gradient-to-r from-zinc-700 to-black px-4 py-2 rounded-md">
-                    https://ardiputra.us.to/post/image
-                </p>
+            <div class="flex flex-col gap-2">
+                <div class="flex gap-2">
+                    <span
+                        class="w-auto whitespace-nowrap rounded-full bg-green-100 px-2.5 py-0.5 text-sm text-green-700">
+                        POST
+                    </span>
+                    <span
+                        class="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                        </svg>
+
+                        <p class="whitespace-nowrap text-sm">Text AI</p>
+                    </span>
+                    <span class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-purple-700">
+                        : JSON
+                    </span>
+                </div>
+                <div
+                    class="relative bg-gradient-to-r from-zinc-700 to-black px-4 py-2 rounded-2xl shadow-lg shadow-black">
+                    <span>https://ardiputra.com/post/text</span>
+                    <button
+                        class="absolute right-2 top-[20%] bg-gradient-to-r from-zinc-700 to-black-vite px-2 rounded-lg border-t border-b border-b-zinc-500">
+                        Request Body
+                    </button>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <div class="flex gap-2">
+                    <span class="w-auto whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-sm text-blue-700">
+                        GET
+                    </span>
+                    <span
+                        class="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                        </svg>
+
+                        <p class="whitespace-nowrap text-sm">Image AI</p>
+                    </span>
+                    <span class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-purple-700">
+                        : String Base64
+                    </span>
+                </div>
+                <div
+                    class="bg-gradient-to-r from-zinc-700 to-black px-4 py-2 rounded-2xl relative shadow-lg shadow-black">
+                    https://ardiputra.com/get/image?<span class="text-yellow-500">key</span>=<span
+                        class="text-blue-vite">WSn6CU0Zsxa0nPhlEK1dct</span>&<span
+                        class="text-yellow-500">prompt</span>=<span class="text-blue-vite">black cat</span>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <div class="flex gap-2">
+                    <span
+                        class="w-auto whitespace-nowrap rounded-full bg-green-100 px-2.5 py-0.5 text-sm text-green-700">
+                        POST
+                    </span>
+                    <span
+                        class="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                        </svg>
+
+                        <p class="whitespace-nowrap text-sm">Image AI</p>
+                    </span>
+                    <span class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-purple-700">
+                        : String Base64
+                    </span>
+                </div>
+                <div
+                    class="relative bg-gradient-to-r from-zinc-700 to-black px-4 py-2 rounded-2xl shadow-lg shadow-black">
+                    https://ardiputra.com/post/image
+                    <button
+                        class="absolute right-2 top-[20%] bg-gradient-to-r from-zinc-700 to-black-vite px-2 rounded-lg border-t border-b border-b-zinc-500">
+                        Request Body
+                    </button>
+                </div>
             </div>
         </div>
         <div
@@ -30,7 +124,8 @@
                 class="inline-flex items-center justify-center rounded-full bg-purple-100 px-2.5 py-0.5 text-purple-700">
                 <p
                     class="whitespace-nowrap text-sm before:w-64 before:h-64 before:bg-gradient-to-br before:from-blue-vite before:from-50% before:to-pink-vite before:to-50% before:absolute before:-z-50 before:top-[50%] before:left-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:blur-[90px]">
-                    WSn6CU0Zsxa0nPhlEK1dct</p>
+                    WSn6CU0Zsxa0nPhlEK1dct
+                </p>
 
                 <button
                     class="-me-1 ms-1.5 inline-block rounded-full bg-purple-200 p-0.5 text-purple-700 transition hover:bg-purple-300">
